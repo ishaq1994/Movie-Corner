58b1f361daef8575ab67589a17080f0a3748e354
Ishaq A Mohamed <mohamedishaq1994@gmail.com>
2021-02-26 20:39:11 +0400

    alignment

--- app/src/main/java/com/ishaq/popularfilm/activities/MainActivity.java
+++ app/src/main/java/com/ishaq/popularfilm/activities/MainActivity.java
@@ -271,6 +271,7 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     protected void onDestroy() {
+
         super.onDestroy();
         PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
     }
